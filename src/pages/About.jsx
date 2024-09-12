56615b9a80d027b1bdf03b630cb252303c774f51
[Furqanali03] <[alif63503@gmail.com]>
2024-09-12 10:00:41 +0500

    improvement

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 function About() {
     return (
       <>
-      <h1 className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">About</h1>
+      <h1 className="px-4 py-2 bg-blue-500 text-center flex justify-center items-center	 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500">About</h1>
       </>
     )
   }
